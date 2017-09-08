38914f5bdb4bdc4282f8ed1d4c935b0b1da911d7
Antoine Nguyen <tonio@ngyn.org>
2017-09-08 18:24:39 +0200

    Initial commit.

---
+++ ngynlabs_odoo/__openerp__.py
@@ -0,0 +1,23 @@
+{
+    'name': 'Ngynlabs document templates',
+    'category': 'Tools',
+    'version': '8.0.1.0.0',
+    'description': """
+Ngynlabs documents
+==================
+
+This module provides templates for documents (invoice and more).
+""",
+    'depends': [
+        'base',
+        'account',
+    ],
+    'installable': True,
+    'data': [
+        'views/external_layout_footer.xml',
+        'views/external_layout_header.xml',
+        'views/report_invoice_document.xml',
+    ],
+    'author': 'Antoine Nguyen, NGYNLABS',
+    'license': 'AGPL-3',
+}
