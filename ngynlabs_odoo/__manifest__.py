0b38afed9cc3366b7da251c126d9c171a4950dd0
Antoine Nguyen <tonio@ngyn.org>
2018-03-29 15:33:22 +0200

    Fixed sale order template.

--- ngynlabs_odoo/__manifest__.py
+++ ngynlabs_odoo/__manifest__.py
@@ -11,12 +11,14 @@ This module provides templates for documents (invoice and more).
     'depends': [
         'base',
         'account',
+        'sale',
     ],
     'installable': True,
     'data': [
         'views/external_layout_footer.xml',
         'views/external_layout_header.xml',
         'views/report_invoice_document.xml',
+        'views/report_sale.xml',
     ],
     'author': 'Antoine Nguyen, NGYNLABS',
     'license': 'AGPL-3',
